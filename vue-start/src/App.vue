7fde77f256238ce94141c31ef98f0f30b77af4cc
simo994q <simonkn1@hotmail.com>
2023-11-10 10:08:36 +0100

    start

---
+++ vue-start/src/App.vue
@@ -0,0 +1,36 @@
+<script>
+import Header from './components/Header.vue';
+
+export default {
+  components: {
+    Header
+  }
+}
+
+</script>
+
+<template>
+  <Header />
+  <main>
+    <h1>Min Vue app</h1>
+    <p>Lavet for at prøve Vue</p>
+  </main>
+</template>
+
+<style lang="scss">
+    main {
+      max-width: 800px;
+      margin: auto;
+
+      h1 {
+        border-bottom: 2px solid #333;
+        padding: 10px 0;
+        color: #333;
+      }
+
+      p {
+        color: #333;
+        margin: 10px 0;
+      }
+    }
+</style>
---
+++ vue-start/src/components/Header.vue
@@ -0,0 +1,37 @@
+<script>
+
+</script>
+
+<template>
+    <div class="headerContainer">
+        <h1>Vue App</h1>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+</template>
+
+<style lang="scss">
+
+.headerContainer {
+    background-color: #333;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 20px;
+
+    h1 {
+        color: #efefef;
+    }
+    ul {
+        display: flex;
+        list-style: none;
+        gap: 10px;
+        color: #efefef;
+        font-size: 20px;
+    }
+}
+
+</style>
