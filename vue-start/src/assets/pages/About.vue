0d980ae65e446cf4254179cc1a27352e358c0d77
simo994q <simonkn1@hotmail.com>
2023-11-10 11:28:21 +0100

    pages

--- vue-start/src/assets/pages/About.vue
+++ vue-start/src/assets/pages/About.vue
@@ -1,4 +1,28 @@
 <script>
+
 </script>
-<template></template>
-<style></style>
+
+<template>
+  <main>
+    <h1>About page</h1>
+    <p>Lavet for at prøve Vue</p>
+  </main>
+</template>
+
+<style lang="scss">
+    main {
+      max-width: 800px;
+      margin: auto;
+
+      h1 {
+        border-bottom: 2px solid #333;
+        padding: 10px 0;
+        color: #333;
+      }
+
+      p {
+        color: #333;
+        margin: 10px 0;
+      }
+    }
+</style>
